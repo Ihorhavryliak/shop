ee677a0acbe1959859351883df8d31780a939876
ihor <kachkamala@gmail.com>
2022-12-01 21:58:14 +0200

    v 9

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { BsCart4 } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
@@ -10,16 +11,16 @@ import { Search } from "../Inputs/Search";
 const Header = React.memo(() => {
   return (
     <header >
-      <div className="navbar navbar-light py-lg-4 pt-3 px-0 pb-0">
+      <div className="navbar navbar-light py-lg-4 pt-3 px-0 pb-0 d-flex justify-content-between ">
       <div className="container">
       <div className="row w-100 align-items-center g-lg-2 g-0">
-        <div className=" col-3 "><NavLink to='/'> 
+        <div className=" col-6 	 col-sm-6 col-md-6 col-lg-3 col-xl-3   col-xxl-3  "><NavLink to='/'> 
         <span className="logo"><BsCart4 className="logo__icon" />  ShopStore  </span></NavLink></div>
-        <div className="col-6 d-none d-lg-block">
+        <div className="col-6 	 col-sm-6 col-md-6 col-lg-6 col-xl-6   col-xxl-6 d-none d-lg-block">
           <Search />
       
         </div>
-        <div className="col-3 text-end fs-4 list-inline-item">
+        <div className="col-6 	 col-sm-6 col-md-6 col-lg-3 col-xl-3   col-xxl-3  text-end fs-4 list-inline-item">
           <a className="text-muted position-relative pe-2" href="s"><AiOutlineHeart /></a>
           <a className="text-muted position-relative pe-2" href="s"><BiUser/> </a>
           <a className="text-muted position-relative" href="s"><FiShoppingBag /></a>  </div>
